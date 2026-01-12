1a82133e6e925f8896c5a9f21b65a10809b7265a
raphaeltkorp <raphael@tkorp.com>
2026-01-12 21:09:59 +0100

    Update gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
 
   return (
     <main className="container">
-      <h1>Welcome to Tauri + React</h1>
+      <h1>Welcome to QoreDB</h1>
 
       <div className="row">
         <a href="https://vite.dev" target="_blank">
